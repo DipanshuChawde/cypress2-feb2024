23b966462614d5455c4430705f2b4cfeedaeff15
DipanshuChawde <dipanshuchoude@gmail.com>
2024-06-26 09:56:53 +0530

    25june

--- cypress/e2e/6-myFiles/1.ksp.cy.js
+++ cypress/e2e/6-myFiles/1.ksp.cy.js
@@ -1,5 +1,5 @@
 ///<reference types="cypress" />
-
+//https://testersdock.com/cypress-new-window/   (for example)
 
 
 describe('verify multitab windows',function(){
@@ -13,11 +13,39 @@ describe('verify multitab windows',function(){
             link=myLink
         })
 
+        // cy.get('a[class="thm-btn"]').eq(2).scrollTo('bottom',{ensureScrollable: false}).invoke('removeAttr','target').click()
+        // cy.url().should('contains','notification/')
+
+
+        cy.window().then(win => {
+            cy.stub(win, 'open').as('pdfOpen');
+          });
+          
+          cy.get('a[class="thm-btn"]').eq(2).scrollTo('bottom',{ensureScrollable: false}).invoke('removeAttr','target').click()
+        //cy.url().should('contains','notification/')
+         cy.get('@pdfOpen').should('be.called');
+          //cy.url().should('contains','notification/')
+
+
+        // cy.intercept('GET', '**/*.pdf').as('getPdf');
+
+        // // Click the link that triggers the PDF load
+        // cy.get('a[class="thm-btn"]').eq(2).scrollTo('bottom',{ensureScrollable: false}).invoke('removeAttr','target').click()
+    
+        // // Wait for the intercepted request and assert its status
+        // cy.wait('@getPdf').then((interception) => {
+        //   expect(interception.response.statusCode).to.eq(200);
+        // });
+     
+
     //   cy.intercept('GEt','/notification/Date_Extention_APC-3064_APC-420.pdf').as('getResourse')
 
 
-     cy.get('a[class="thm-btn"]').eq(2).scrollTo('bottom',{ensureScrollable: false}).invoke('removeAttr','target').click({ timeout: 120000 })
-     cy.url().should('contains','notification/')
+     
+   
+    
+    
+    //cy.url().should('contains','notification/')
     //     cy.wait('@getResourse')
         // cy.get('a[class="thm-btn"]').eq(2).scrollTo('bottom',{ensureScrollable: false}).invoke('attr','href').then(myLink => {
         //     cy.log(myLink);
